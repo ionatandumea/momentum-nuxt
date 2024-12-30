7acb03dfe3b77b2c4228a28ab023431f35144ec3
Ionatan Dumea <134598668+ionatandumea@users.noreply.github.com>
2024-12-30 19:54:01 +0200

    feat: add runtime configuration for public API URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   srcDir: "src/",
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+        apiUrl: process.env.API_URL
+    }
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@primevue/nuxt-module",
